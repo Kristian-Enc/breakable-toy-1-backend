7b590d6d3af60f2a2f839b3066b580cf5ae832fd
kristian.hernandez@encora.com <kristian.hernandez@ENCEDCREMLT0781.local>
2025-05-13 23:09:13 -0600

    Add ProductRepository interface

---
+++ src/main/java/com/kristian/inventory/repository/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.kristian.inventory.repository;
+
+import com.kristian.inventory.model.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+
+    List<Product> findAll();
+
+    Optional<Product> findById(Long id);
+
+    Product save(Product product);
+
+    void deleteById(Long id);
+
+}
