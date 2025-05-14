f581dd826b9599c035e73f770de3accba49c1ed4
kristian.hernandez@encora.com <kristian.hernandez@ENCEDCREMLT0781.local>
2025-05-13 21:40:51 -0600

    Set up Spring Boot and project structure

---
+++ src/main/java/com/kristian/inventory/model/Product.java
@@ -0,0 +1,134 @@
+package com.kristian.inventory.model;
+
+/*
+
+            ----- Model -----
+
+ -- A product should have the following properties:
+
+- Id (unique)
+- Name (required, max 120 characters)
+- Category (required)
+- Unit price (required)
+- Expiration date (optional)
+- Quantity in stock (required)
+- Creation date
+- Update date
+
+
+*/
+
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Product {
+
+    // Fields
+    private Long id;
+    private String name;
+    private String category;
+    private BigDecimal unitPrice;
+    private int quantityInStock;
+    private LocalDate expirationDate;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    // Default Constructor
+    public Product(){
+
+    }
+    // All arguments constructor
+    public Product(Long id, String name, String category, BigDecimal unitPrice, int quantityInStock, LocalDate expirationDate, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.unitPrice = unitPrice;
+        this.quantityInStock = quantityInStock;
+        this.expirationDate = expirationDate;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    // Getter
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public int getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    // Setter
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", quantityInStock=" + quantityInStock +
+                ", expirationDate=" + expirationDate +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}
